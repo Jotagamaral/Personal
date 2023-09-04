922365505cad581c6a3a618600efd7451f8b10be
João Gabriel <143552261+Jotagamaral@users.noreply.github.com>
2023-09-04 18:30:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+async function clicar() {
+
+    fetch('pasta/piadas.json')
+    .then(resposta => resposta.json())
+    .then(piadas => {
+        let idx = Math.floor((Math.random() * 10))
+        let piada = piadas[idx]
+        
+       
+        document.getElementsByClassName('awnser').innerHTML = 'show'
+        document.getElementById('piada').innerHTML = piada.piada
+        document.getElementById('resposta').innerHTML = piada.resposta
+    })
+   
+}
+
+
+
+
+
+
